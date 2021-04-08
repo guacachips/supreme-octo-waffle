28de5cb11c5a1fc549cba409aace14fa965f13b1
Christopher MONTOYA <contact@christopher-montoya.com>
2021-04-07 22:52:25 -0400

    feat(ngx-sow-library): move card component from demo to library

--- projects/ngx-sow-library/src/lib/ngx-sow-library.module.ts
+++ projects/ngx-sow-library/src/lib/ngx-sow-library.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { NgxSowLibraryComponent } from './ngx-sow-library.component';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
   declarations: [
-    NgxSowLibraryComponent
+    NgxSowLibraryComponent,
+    CardComponent
   ],
   imports: [
   ],
