fe57e723258d957c58cc55354182dc3c0c094645
Christopher MONTOYA <contact@christopher-montoya.com>
2021-04-12 16:23:47 -0400

    fix(lib-elements): add shadow dom encapsulation

--- ngx-design-system/projects/lib-elements/src/lib/components/card/card.component.ts
+++ ngx-design-system/projects/lib-elements/src/lib/components/card/card.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'lib-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class CardComponent implements OnInit {
 
